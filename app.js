d3579a791205b496f59ffde0009289a921acbdba
Craig Moynes <cmoynes@ca.ibm.com>
2019-06-26 10:53:25 -0400

    Initial Server implementation with logging

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const 
-debug = require('debug')('scientist'),
-argv = require('yargs')
+debug   = require('debug')('scientist'),
+http    = require('http'),
+url     = require('url'),
+argv    = require('yargs')
     .alias('help', 'h')
     .alias('help', '?')
     .command(require('./command-module'))
@@ -36,3 +38,49 @@ if (argv.add){
     })
 }
 
+var server = http.createServer( (req, res) => {
+    debug('Incoming Request: %o', req.url)
+    let parsedUrl = url.parse(req.url, true);
+    debug('Incoming Request Path: %o', parsedUrl.pathname)
+    debug('Incoming Request Headers: %o', req.headers)
+
+    let controlRequestOptions = constructionOptions(argv.controlHost, req.url.toString(), req.headers)
+    let candidateRequestOptions = controlRequestOptions
+
+    // if candidateHost specified, use that for candidate request
+    if (argv.candidateHost)
+        candidateRequestOptions.baseURL = argv.candidateHost
+    // if URL replacement specified, perform replacement on url
+    if (argv.replace){
+        candidateRequestOptions.url = candidateRequestOptions.url.replace(argv.replace[0], argv.replace[1])
+    }
+    // if candidate header deletion specified
+    if (argv.delete){
+        argv.delete.forEach(key => {
+            delete candidateRequestOptions.headers[key]
+        })
+    }
+    //if candidate header addition specified
+    if (argv.add){
+        argv.add.forEach(elem => {
+            let keyval = elem.split(':', 2)
+            candidateRequestOptions.headers[keyval[0]] = keyval[1]
+        })
+    }
+
+})
+
+server.listen( port )
+console.log(`\nListening on port ${port}`)
+
+
+
+function constructionOptions(hostname, path, requestHeaders){
+    delete requestHeaders['host'] //remove the scientist service host
+    let options = {
+        baseURL: hostname,
+        url: path,
+        headers: requestHeaders
+    }
+    return options
+}
