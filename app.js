6808bc302c4d70eca766c71251f88ce2336ac474
Craig Moynes <cmoynes@ca.ibm.com>
2019-06-26 09:21:20 -0400

    Cleanup Yargs to prep for extract
    Prepping to extact to own command module

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const argv = require('yargs')
     .alias('help', 'h')
     .alias('help', '?')
-    .command(['run <control-host> [candidate-host] [options]', '$0 <control-host> [candidate-host] [options]'], 'run scientist', (yargs) => {
-        yargs.positional('control-host', {
-            'describe': 'Valid URL for the control host (known good service)',
-            'coerce': function(host) {
-                new URL(host) //will throw error if invalid host
-                return host   //we dont want to use the URL internally
-            }
-        }).positional('candidate-host', {
-            'describe': 'Valid URL for the candidate host (proposed service with changes). If not provided, control-host will be used for candidate requests',
-            'coerce': function(host) {
-                new URL(host) // will throw error if invalid host
-                return host   //we dont want to use the URL internally
-            }
-        })        
-        .describe('replace', '"<regexp|string> <replacement-string>" replaces the regular expression or string match in the path with the replace-string for candidate requests')
-        .alias('replace', 'r')
-        .nargs('replace', 2) //2 arguments required for 'replace'
-        .coerce('replace', function(arg) {
+    .command({
+        command: ['run <control-host> [candidate-host] [options]', '$0 <control-host> [candidate-host] [options]'],
+        builder: yargs => {
+            yargs.positional('control-host', {
+                describe: 'Valid URL for the control host (known good service)',
+                coerce: function(host) {
+                    new URL(host) //will throw error if invalid host
+                    return host   //we dont want to use the URL internally
+                }
+            }).positional('candidate-host', {
+                'describe': 'Valid URL for the candidate host (proposed service with changes). If not provided, control-host will be used for candidate requests',
+                'coerce': function(host) {
+                    new URL(host) // will throw error if invalid host
+                    return host   //we dont want to use the URL internally
+                }
+            })
+        }
+    })
+    .option('replace', {
+        describe: '"<regexp|string> <replacement-string>" replaces the regular expression or string match in the path with the replace-string for candidate requests',
+        alias: 'r',
+        nargs: 2, //2 arguments required for 'replace'
+        coerce: function(arg) {
             //test supplied regexp|string to see if it is a valid regexp 
             //and replace argument with compiled regexp if it is
             try{ 
@@ -29,17 +34,20 @@ const argv = require('yargs')
                 console.error(err) 
             }
             return arg
-        })
-        .describe('delete', "'key' deletes the specified header from Candidate requests. Multiple delete options may be used. Header deletions occur before add additions")
-        .alias('delete', 'd')
-        .alias('delete', 'delete-header')
-        .nargs('delete', 1) //deletion requires 1 arg for header key
-
-        .describe('add', "'key: value' adds a header to the Candidate request. Multiple add options may be used")
-        .alias('add', ['a','add-header'])
-        .nargs('add', 1) //add header requires 1 arg, a string with "key: value"
+        }
+    })
+    .option('delete', {
+        describe: "'key' deletes the specified header from Candidate requests. Multiple delete options may be used. Header deletions occur before add additions",
+        alias: ['d','delete-header'],
+        nargs: 1, //deletion requires 1 arg for header key
+    })
+    .option('add', {
+        describe: "'key: value' adds a header to the Candidate request. Multiple add options may be used",
+        alias: ['a','add-header'],
+        nargs: 1, //add header requires 1 arg, a string with "key: value"
         //TODO coerce for checking it is in format key: value
     })
+    .strict()
     .example('$0 http://host1 http://host2', 'All requests received will be sent first to the Control host (host1), then to Candidate host (host2) and the results compared')
     .example('$0 http://host1 -r v1 v2', 'Control requests will be sent to the v1 endpoint, Candidate requests will be sent to the v2 endpoint on the same host')
     .example('$0 http://host1 http://host2 -r /v. /v2', 'Control requests will be sent to the v1 endpoint, Candidate requests will be sent to the V2 endpoint. The regexp matches any thing like /v.')
