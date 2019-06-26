23f03a3c79da750afbea2cd0cdcb45d0b3ced14c
Craig Moynes <cmoynes@ca.ibm.com>
2019-06-26 10:35:02 -0400

    Check parameters and logging
    Add service logging for replace url, delete, and add headers
    
    This logging happens when the service starts up as a visual check
    for users to see that expectations
    match reality

--- app.js
+++ app.js
@@ -7,3 +7,32 @@ argv = require('yargs')
     .argv
     
 debug(argv)
+
+const port = process.env["NODE_PORT"] | '8080'
+debug(`port ${port}`)
+
+//Hack to disable certificate check for https
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
+console.log(`Control Host: ${argv.controlHost}`)
+console.log(`Candidate Host: ${argv.candidateHost || argv.controlHost}`)
+
+if (argv.replace){
+    console.log(`Path Replacement: Candidate requests with '${argv.replace[0]}' in the path will be replaced with '${argv.replace[1]}'`)
+} else {
+    console.log('No Path Replacement specified')
+}
+
+if (argv.delete){
+    argv.delete.forEach( (element) => {
+        console.log(`Deleting header from Candidate requests: (${element})`)
+    });
+} 
+
+if (argv.add){
+    argv.add.forEach( (element) => {
+        var keyval = element.split(':', 2)
+        console.log(`Adding header to Candidate requests key:(${keyval[0]}) value(${keyval[1]})`)
+    })
+}
+
