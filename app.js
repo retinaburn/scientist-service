bd686b12a5b8795d83a075c81095e5caa53e3d95
Craig Moynes <cmoynes@ca.ibm.com>
2019-06-26 09:52:46 -0400

    Add debug for logging

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const argv = require('yargs')
+const 
+debug = require('debug')('scientist'),
+argv = require('yargs')
     .alias('help', 'h')
     .alias('help', '?')
     .command(require('./command-module'))
     .argv
-
-console.log(argv)
+    
+debug(argv)
